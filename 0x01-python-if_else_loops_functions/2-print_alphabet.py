8f492c3821ff2b474886745a05adb2cdd398059a
Yulaco <cleochen93@gmail.com>
2024-01-04 02:09:10 -0800

    Alphabet lowercase

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-for alphabet in range (97, 123):
-    if ((alphabet >= 97) and (alphabet < 123)):
-        print(chr(alphabet), end="")
+for alphabet in range(97, 123):
+    print(chr(alphabet), end="")

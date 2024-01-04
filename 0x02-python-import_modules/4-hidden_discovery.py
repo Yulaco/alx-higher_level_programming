09e1b237ddee52ad12d5bd9ace0c8577d4e3b38a
Yulaco <cleochen93@gmail.com>
2024-01-04 14:26:51 -0800

    Program prints names defined by hidden file

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import hidden_4 
+    import hidden_4
+
+names = dir(hidden_4)
 
 for a in names:
-    if (names != "__"):
-    print("{}".format(sort(names)))
+    if (a != "__"):
+        print("{}".format(a))

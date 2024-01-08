d0b529e40bea6245c9a24ecba86408b23fd069a1
Yulaco <cleochen93@gmail.com>
2024-01-08 12:53:25 -0800

    Removes c and C

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Removes all characters c and C
+
+def no_c(my_string):
+
+    new_string = ""
+    for a in my_string:
+        if my_string == 'c' or my_string == 'C':
+            my_string.remove(c, C)
+    return(my_string)

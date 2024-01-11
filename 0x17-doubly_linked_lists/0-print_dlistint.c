85bbd466da2448e5881110b32c1375104ab447ff
Yulaco <cleochen93@gmail.com>
2024-01-11 21:26:07 +0000

    prints all the elements of a list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,12 +10,12 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	size_t node = 0;
 
-	do
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
 		node++;
-	} while (h != NULL);
+	}
 
 	return (node);
 }

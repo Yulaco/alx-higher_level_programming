9faf7def26f5ad98d60c2cb56c4ee94f862a0ed5
Yulaco <cleochen93@gmail.com>
2024-03-14 14:31:38 -0700

    Lists all cities

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,8 @@
+-- Lists all the cities of California that can be found in the database hbtn_0d_usa.
+SELECT id, name
+FROM cities
+WHERE stae_id = (
+	SELECT id
+	FROM states
+	WHERE name = 'California'
+);
